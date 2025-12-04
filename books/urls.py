0c7a95fb3e034f3c1d286c00098d63952a4903f9
JolanaB169 <Jola.T@seznam.cz>
2025-12-04 08:57:37 +0100

    basic templates

---
+++ books/urls.py
@@ -0,0 +1,14 @@
+from django.urls import path
+from .views.home import home_page
+# from .views.books import book_list, book_detail
+# from .views.authors import author_detail, author_list
+
+urlpatterns = [
+    path('', home_page, name='home_page'),
+    # path('books/', book_list, name='book_list'),
+    # path('book/<int:pk>/', book_detail, name='book_detail'),
+    # path('author/<int:pk>/', author_detail, name='author_detail'),
+    # path('authors/', author_list, name='author_list'),
+
+
+]
