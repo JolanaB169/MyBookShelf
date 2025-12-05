e89194a89e160b2a73c51933e3e1ed386ca8b722
JolanaB169 <Jola.T@seznam.cz>
2025-12-05 18:00:46 +0100

    login, register, logout

--- books/urls.py
+++ books/urls.py
@@ -4,6 +4,9 @@ from .views.search_books_view import search_books_view
 from .views.book_detail import book_detail_view
 from .views.author_detail import author_detail_view
 from .views.search_author import search_author_view
+from .views.logout import logout_view
+from .views.login import login_view
+from .views.register import register_view
 
 
 
@@ -13,6 +16,10 @@ urlpatterns = [
     path('book/<str:book_id>/', book_detail_view, name='book_detail'),
     path('author/<str:author_name>/', author_detail_view, name='author_detail'),
     path('author-search/', search_author_view, name='author_search'),
+
+    path('login/', login_view, name='login'),
+    path('logout/', logout_view, name='logout'),
+    path('register/', register_view, name='register'),
 
 
 ]
--- books/views/__init__.py
+++ books/views/__init__.py
@@ -1,3 +1,5 @@
 from .home import home_page
 from .search_books_view import search_books_view
 from .book_detail import book_detail_view
+from .search_author import search_author_view
+from .author_detail import author_detail_view
